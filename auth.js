26fd93343a5e9b60e6eeab49205d1b2955e44eb9
artdobro <105646581+artdobro@users.noreply.github.com>
2026-01-04 23:49:29 +0200

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,15 @@
+function getAuthUser() {
+  return JSON.parse(localStorage.getItem("authUser"));
+}
+
+function isLoggedIn() {
+  return !!getAuthUser();
+}
+
+function logout() {
+  localStorage.removeItem("authUser");
+}
+function isAdmin() {
+  const user = getAuthUser();
+  return user && user.role === "admin";
+}
