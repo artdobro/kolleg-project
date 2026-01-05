ca100558e77f9d1ffccdb2c4813dbb7c4a48eef4
artdobro <105646581+artdobro@users.noreply.github.com>
2026-01-05 17:52:25 +0200

    Update adding.js

--- adding.js
+++ adding.js
@@ -1,3 +1,18 @@
+let lastScrollTop = 0;
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', function() {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop < lastScrollTop) {
+    // Прокрутка вверх
+    header.classList.add('visible');
+  } else {
+    // Прокрутка вниз
+    header.classList.remove('visible');
+  }
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Для iOS
+});
+
 const profileBlock = document.getElementById("profileBlock");
 const profileName = document.getElementById("profileName");
 const logoutBtn = document.getElementById("logoutBtn");
